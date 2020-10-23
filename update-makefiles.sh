P-404/android_hardware_404_interfaces

404/interfaces: Initial commit
Change-Id: I87ac8d1b6a31fa71855108067c18b742faf00af8

update-makefiles.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.p404:hardware/404/interfaces" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"

404-Mess/interface

update-makefiles.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+
+source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.lineage:hardware/404/interfaces" \
+  "android.hardware:hardware/interfaces" \
+  "android.hidl:system/libhidl/transport"